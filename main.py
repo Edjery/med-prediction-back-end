1a5ffeafdbdf981299515106f97cb2a780f12366
Edjery Gumbao <edisgumbao@gmail.com>
2024-02-01 10:13:41 +0800

    initial commit, install fast api and uvicorn then added initial contents of main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {
+        "Hello There!": "Welcome to the Server!", 
+        "For more info": "Please add '/docs' after the url", 
+        "example": "http://127.0.0.1:8000/docs"
+    }
+
+@app.get("/predict/{text}")
+async def predict(text: str):
+    return {"text": text}
