613e3c4ba53a135a3c6c75d41b0607071e012233
Edjery Gumbao <edisgumbao@gmail.com>
2024-02-01 11:03:29 +0800

    updated middleware

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins={"*"},
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 @app.get("/")
